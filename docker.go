9eff0153a8bfed92a0f09768d12d986f4eb593d1
skanehira <sho19921005@gmail.com>
2018-09-10 07:41:29 +0900

    create container

--- docker.go
+++ docker.go
@@ -1,6 +1,8 @@
 package main
 
-import docker "github.com/fsouza/go-dockerclient"
+import (
+	docker "github.com/fsouza/go-dockerclient"
+)
 
 const (
 	endpoint = "unix:///var/run/docker.sock"
@@ -66,4 +68,64 @@ func (d *Docker) ContainersWithOptions(options docker.ListContainersOptions) []d
 		panic(err)
 	}
 	return cns
+}
+
+func (d *Docker) CreateContainerWithOptions(config map[string]string) error {
+	_, err := d.Client.CreateContainer(NewContainerOptions(config))
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewContainerOptions(config map[string]string) docker.CreateContainerOptions {
+
+	options := docker.CreateContainerOptions{
+		Config:     new(docker.Config),
+		HostConfig: new(docker.HostConfig),
+	}
+
+	if name := config["Name"]; name != "" {
+		options.Name = name
+	}
+
+	if image := config["Image"]; image != "" {
+		options.Config.Image = image
+	}
+
+	if port := config["Port"]; port != "" {
+		options.Config.ExposedPorts = map[docker.Port]struct{}{
+			docker.Port(port): struct{}{},
+		}
+
+		if hostPort := config["HostPort"]; hostPort != "" {
+			options.HostConfig.PortBindings = map[docker.Port][]docker.PortBinding{
+				docker.Port(port): []docker.PortBinding{
+					docker.PortBinding{
+						HostIP:   "0.0.0.0",
+						HostPort: hostPort,
+					},
+				},
+			}
+		}
+	}
+
+	if env := config["Env"]; env != "" {
+		options.Config.Env = []string{env}
+	}
+
+	if hostVolume := config["HostVolume"]; hostVolume != "" {
+		if volume := config["Volume"]; volume != "" {
+			options.HostConfig.Mounts = []docker.HostMount{
+				docker.HostMount{
+					Target: volume,
+					Source: hostVolume,
+					Type:   "bind",
+				},
+			}
+		}
+	}
+
+	return options
 }
