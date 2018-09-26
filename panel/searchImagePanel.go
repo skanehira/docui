fb3908ae77286704e5a034466f208d388fe4121b
skanehira <sho19921005@gmail.com>
2018-09-26 17:20:10 +0900

    fix input move cursor left/right

--- panel/searchImagePanel.go
+++ panel/searchImagePanel.go
@@ -52,7 +52,6 @@ func (s *SearchImage) SetView(g *gocui.Gui) error {
 		v.Autoscroll = true
 		v.Editable = true
 		v.Wrap = true
-		v.Editor = s
 	}
 
 	s.SwitchPanel(v.Name())
@@ -140,21 +139,6 @@ func (s *SearchImage) SearchImage(g *gocui.Gui, v *gocui.View) error {
 	}
 
 	return nil
-}
-
-func (s *SearchImage) Edit(v *gocui.View, key gocui.Key, ch rune, mod gocui.Modifier) {
-	switch {
-	case ch != 0 && mod == 0:
-		v.EditWrite(ch)
-	case key == gocui.KeySpace:
-		v.EditWrite(' ')
-	case key == gocui.KeyBackspace || key == gocui.KeyBackspace2:
-		v.EditDelete(true)
-	case key == gocui.KeyArrowLeft:
-		v.MoveCursor(-1, 0, false)
-	case key == gocui.KeyArrowRight:
-		v.MoveCursor(+1, 0, false)
-	}
 }
 
 func (s *SearchImage) ClosePanel(g *gocui.Gui, v *gocui.View) error {
