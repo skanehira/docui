8cd7b18eaa7d0d37f19bb95a57a9c6674ce3790e
skanehira <sho19921005@gmail.com>
2018-10-03 06:45:22 +0900

    fix build.sh

--- build/build.sh
+++ build/build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 # build docker image
-docker build -t docui .
+docker build -t skanehira/docui .
 
 # remove build image
 docker rmi $(docker images --filter "dangling=true" -aq)
+
+# push image to dockerr hub
+docker push skanehira/docui
