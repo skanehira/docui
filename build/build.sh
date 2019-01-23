d492ed13f6cecb0ee2fe356fd0f9069b9002e858
skanehira <sho19921005@gmail.com>
2019-01-23 19:27:22 +0900

    Fixed Can't attach container when no docker client on container #50

--- build/build.sh
+++ build/build.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# download docker client only
+DOCKER_CLI_VERSION="18.09.1"
+DOWNLOAD_URL="https://download.docker.com/linux/static/stable/x86_64/docker-$DOCKER_CLI_VERSION.tgz"
+
+curl -L $DOWNLOAD_URL | tar -xz
+
 # build docui binary
 GOOS=linux GOARCH=amd64 go build ../
 
@@ -7,7 +13,7 @@ GOOS=linux GOARCH=amd64 go build ../
 docker build -t skanehira/docui .
 
 # remove docui binary
-rm -rf ./docui
+rm -rf ./docui docker
 
 # push image to dockerr hub
 docker push skanehira/docui
