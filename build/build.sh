488811a37993948957ccefd1ad26a5523e23bbd4
skanehira <sho19921005@gmail.com>
2019-01-26 02:29:19 +0900

    Fixed docker image build.sh #52

--- build/build.sh
+++ build/build.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
-
-# download docker client only
-DOCKER_CLI_VERSION="18.09.1"
-DOWNLOAD_URL="https://download.docker.com/linux/static/stable/x86_64/docker-$DOCKER_CLI_VERSION.tgz"
-
-curl -L $DOWNLOAD_URL | tar -xz
-
 # build docui binary
 GOOS=linux GOARCH=amd64 go build ../
 
