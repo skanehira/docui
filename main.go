e3f05bcabb1ed20aa780bf441e11376c13740023
skanehira <sho19921005@gmail.com>
2019-02-07 09:42:27 +0900

    add some comment and move vars

--- main.go
+++ main.go
@@ -11,20 +11,23 @@ import (
 	"github.com/skanehira/docui/panel"
 )
 
+var (
+	endpoint = flag.String("endpoint", "unix:///var/run/docker.sock", "Docker endpoint")
+	cert     = flag.String("cert", "", "cert.pem file path")
+	key      = flag.String("key", "", "key.pem file path")
+	ca       = flag.String("ca", "", "ca.pem file path")
+)
+
 func main() {
 	// if terminal window size is not zero
 	if !common.IsTerminalWindowSizeThanZero() {
 		return
 	}
 
-	var (
-		endpoint = flag.String("endpoint", "unix:///var/run/docker.sock", "Docker endpoint")
-		cert     = flag.String("cert", "", "cert.pem file path")
-		key      = flag.String("key", "", "key.pem file path")
-		ca       = flag.String("ca", "", "ca.pem file path")
-	)
+	// parse flag
 	flag.Parse()
 
+	// new dcoker client
 	config := docker.NewClientConfig(*endpoint, *cert, *key, *ca)
 	dockerClient := docker.NewDocker(config)
 
@@ -35,16 +38,21 @@ func main() {
 	}
 
 	for {
+		// create new panel
 		gui := panel.New(gocui.Output256, dockerClient)
 		gui.Logger.Info("docui start")
+
+		// run docui
 		err := gui.MainLoop()
 
 		switch err {
 		case gocui.ErrQuit:
+			// exit
 			gui.Logger.Info("docui finished")
 			gui.Close()
 			os.Exit(0)
 		case panel.ExecFlag:
+			// when exec container gui will return ExecFlag
 			gui.Gui.Close()
 			gui.Panels[panel.ContainerListPanel].(*panel.ContainerList).Exec()
 		}
