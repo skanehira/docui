244f11456649403b4b990d327b784aaa693f1db5
Masayuki Goto <originals0711@gmail.com>
2019-02-12 07:35:02 +0900

    fix var names of func

--- common/common.go
+++ common/common.go
@@ -224,18 +224,18 @@ func IsTerminalWindowSizeThanZero() bool {
 }
 
 // IsValidPanelSize checks if a panel size is valid.
-func IsValidPanelSize(x0, y0, x1, y1 int) error {
-	if x0 >= x1 || y0 >= y1 {
+func IsValidPanelSize(x, y, w, h int) error {
+	if x >= w || y >= h {
 		return ErrSmallTerminalWindowSize
 	}
 	return nil
 }
 
 // SetViewWithValidPanelSize run SetView with a valid panel size.
-func SetViewWithValidPanelSize(g *gocui.Gui, name string, x0, y0, x1, y1 int) (*gocui.View, error) {
-	if err := IsValidPanelSize(x0, y0, x1, y1); err != nil {
+func SetViewWithValidPanelSize(g *gocui.Gui, name string, x, y, w, h int) (*gocui.View, error) {
+	if err := IsValidPanelSize(x, y, w, h); err != nil {
 		panic(err)
 	}
-	v, err := g.SetView(name, x0, y0, x1, y1)
+	v, err := g.SetView(name, x, y, w, h)
 	return v, err
 }
